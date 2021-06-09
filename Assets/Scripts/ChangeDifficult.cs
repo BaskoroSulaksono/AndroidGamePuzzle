4559100e3a9a322ccb395ca93ba7bff7ffc2c488
BaskoroSulaskono <mbsulaksono@gmail.com>
2021-06-09 20:23:57 +0700

    bisa ganti level belum bisa balik
    ganti level sukses tapi belum bisa kembali dan belum ada indikatornya

---
+++ Assets/Scripts/ChangeDifficult.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeDifficult : MonoBehaviour
+{
+    public string easySceneLevel, mediumSceneLevel;
+    private string nextScene;
+    public Text btnDifficult;
+    //TEST SCRIPT
+    public void ChangeDiff()
+    {
+        // checking current scene
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == easySceneLevel)
+        {
+            nextScene = mediumSceneLevel;
+        }
+        else
+        {
+            nextScene = easySceneLevel;
+        }
+        SceneManager.LoadScene(nextScene);
+        
+    }
+    public void UpdateUI()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == easySceneLevel)
+        {
+            btnDifficult.text = "Easy";
+        }
+        else
+        {
+            btnDifficult.text = "Medium";
+        }
+    }
+    
+}
